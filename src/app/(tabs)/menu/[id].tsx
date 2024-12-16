a0a926f2fcba51539f3c7cd531e52a2dd4e30b04
zakariyya <wang033172@gmail.com>
2024-12-16 23:05:06 +0800

    detailing

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
+import products from '@assets/data/products';
 
 export default function ProductDetailsScreen() {
   const { id } = useLocalSearchParams();
+  const product = products.find((i) => String(i.id) == id);  
+  const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+  if(!product){
+    return <Text>Product not Found</Text>
+  }
+  
   return (
     <View>
-      <Stack.Screen options={{title: 'Details'}}/>
-      <Text style={{fontSize:20}}>ProductDetailsScreen: {id}</Text>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image source={{uri: product.image || defaultPizzaImage}}/>
+      <Text style={{ fontSize: 20 }}>ProductDetailsScreen: {id}</Text>
     </View>
   )
 }
