fb784f50040d60798ee3d112cbd9c51427524fac
zakariyya <wang033172@gmail.com>
2024-12-17 23:10:58 +0800

    choose size

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,24 +1,82 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import products from '@assets/data/products';
 
+type SizeProp = 'S' | 'M' | 'L' | 'XL';
+
 export default function ProductDetailsScreen() {
   const { id } = useLocalSearchParams();
-  const product = products.find((i) => String(i.id) == id);  
+  const product = products.find((i) => String(i.id) == id);
   const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
-  if(!product){
+  const sizes: SizeProp[] = ['S', 'M', 'L', 'XL'];
+  const [currentSize, setCurrentSize] = useState<SizeProp>('S');
+
+  if (!product) {
     return <Text>Product not Found</Text>
   }
-  
+
   return (
-    <View>
+    <View style={styles.container}>
       <Stack.Screen options={{ title: product.name }} />
-      <Image source={{uri: product.image || defaultPizzaImage}}/>
-      <Text style={{ fontSize: 20 }}>ProductDetailsScreen: {id}</Text>
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((item, index) => (
+          <Pressable key={index} onPress={() => setCurrentSize(item)}>
+            {item === currentSize ?
+              <View style={styles.selectedSize}>
+                <Text style={styles.sizeText}>{item}</Text>
+              </View>
+              :
+              <View style={styles.size}>
+                <Text style={styles.sizeText}>{item}</Text>
+              </View>
+            }
+          </Pressable>
+        ))}
+      </View>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1
+  },
+  price: {
+    fontWeight: 'bold'
+  },
+  sizes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10
+  },
+  selectedSize: {
+    backgroundColor: 'gainsboro',
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  size: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: 500
+  }
+})
